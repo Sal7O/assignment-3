b7c5e6fbfcef892c4107c611d332b1143e36f3d2
MahmoudSaleh22 <mahmoud22saleh22@gmail.com>
2023-12-06 18:34:22 +0200

    problem 3

---
+++ problem-3.cxx
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cctype>
+#include <fstream>
+#include <map>
+#include <set>
+using namespace std;
+int main()
+{
+    // enter file path
+    string filePath;
+    cin >> filePath;
+
+    // open file to read words
+    ifstream file (filePath);
+
+
+    if (file.is_open()) //check if file is open
+    {
+        // create a map and a set to save words and number of occurrence
+        map <string, int> count;
+        set <string> list;
+
+
+        string word, new_word; // words read from file
+
+        while (file >> word)
+        {
+            new_word = "";
+            for (int i = 0; i <word.size(); i++)
+            {
+                if (isalnum(word[i]))//check the punctuation
+                {
+                    new_word.push_back(tolower(word[i])); // convert all letters to lowercase
+
+                }
+
+            }
+
+            if (list.count(new_word) != 0) // check if the word is saved in the list
+            {
+                //if not saved, insert it to both map and set (with value 1 for map)
+                count[new_word] = 1;
+                list.insert(new_word);
+            }
+            else
+                count[new_word]++;//increment the number in the map
+        }
+        file.close(); // close the file after reading
+
+        // output the frequency table
+        for (const auto& pair : count)
+        {
+
+            string w = pair.first;
+            int frequency = pair.second;
+            cout << w << " : " << frequency << "\n";
+        }
+    }
+    else
+    {
+        cout << "file path is not true!";
+    }
+
+}
